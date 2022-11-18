580ccca42de711fefb96ed785e3b2863f81bfa63
Michał Śledź <michalsledz34@gmail.com>
2022-11-18 12:39:23 +0100

    Remove unused code

--- test/membrane_element_audiometer/peakmeter_test.exs
+++ test/membrane_element_audiometer/peakmeter_test.exs
@@ -10,19 +10,6 @@ defmodule Membrane.Audiometer.PeakmeterTest do
   test "integration" do
     data = [1, 2, 3, 2, 1] |> Enum.map(&<<&1>>)
 
-    # {:ok, pipeline} =
-      # %Testing.Pipeline.Options{
-      #   elements: [
-      #     source: %Testing.Source{
-      #       output: data,
-      #       stream_format: %RawAudio{channels: 1, sample_rate: 44_100, sample_format: :s16le}
-      #     },
-      #     peakmeter: @module,
-      #     sink: Testing.Sink
-      #   ]
-      # }
-      # |> Testing.Pipeline.start_link_supervised!()
-
     links = [
       child(:source, %Testing.Source{
         output: data,
@@ -46,7 +33,5 @@ defmodule Membrane.Audiometer.PeakmeterTest do
       assert_sink_buffer(pipeline, :sink, %Membrane.Buffer{payload: received_payload})
       assert payload == received_payload
     end)
-
-    Testing.Pipeline.terminate(pipeline, blocking?: true)
   end
 end
