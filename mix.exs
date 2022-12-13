c176d8c8911953d3a007b193a76cd09d3e236b95
Michał Śledź <michalsledz34@gmail.com>
2022-12-13 17:38:09 +0100

    Don't defer peakmeter termination (#14)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Audiometer.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.10.0"
+  @version "0.10.1"
   @github_url "https://github.com/membraneframework/membrane_audiometer_plugin"
 
   def project do
