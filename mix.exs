c49e4cee8bd1e1ee7b2e70b49c64d0c22e577c36
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-11-08 17:17:20 +0100

    bump to 0.12.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Audiometer.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.11.1"
+  @version "0.12.0"
   @github_url "https://github.com/membraneframework/membrane_audiometer_plugin"
 
   def project do
@@ -70,8 +70,8 @@ defmodule Membrane.Audiometer.Plugin.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.12.0"},
-      {:membrane_raw_audio_format, "0.11.0"},
+      {:membrane_core, "~> 1.0"},
+      {:membrane_raw_audio_format, "~> 0.12.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false}
